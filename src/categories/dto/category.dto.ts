8fce7d1e062244c51ccf5c05ce06608b3c47ff95
Geethma Chamath <geethmachamath7@gmail.com>
2026-01-08 14:08:09 +0530

    add some comments

--- src/categories/dto/category.dto.ts
+++ src/categories/dto/category.dto.ts
@@ -53,3 +53,7 @@ export class UpdateCategoryDto {
   @Transform(({ value }) => parseInt(value))
   sortOrder?: number;
 }
+
+//required means must exist(no @IsOptional()) and cannot be null or undefined.
+//optional means may or may not exist(can be undefined) but if it exists, it cannot be null.
+//if the request HAS an extra field NOT in the DTO, it will be IGNORED and NOT cause an error.(whitelist: true Removes fields that are NOT in the DTO)
